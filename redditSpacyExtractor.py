d1b956dce157f9e590037c0d527b7a16538b922e
TarunaDatta <tchittajallu@gmail.com>
2019-10-09 21:55:36 +0530

    Added a variable "old_start_at" to ensure the "new_start_at" doesn't get stuck at the same value in the loop in the get_comments method. This is to ensure the method doesn't go into an infinite loop Removed the after parameter from the __init__ constructor Removed the after parameter from the URI_from_fields method and its corresponding if code block Added a variable filtered_collections to the get_comments method to store the retrieved comments after being validated by the validCommentFilter method

--- redditSpacyExtractor.py
+++ redditSpacyExtractor.py
@@ -13,22 +13,23 @@ class RedditSpacyExtractor:
     def __init__(self,
                  subreddit=None,
                  min_score=5,
+                 min_len=10,
                  min_sent_len=5,
                  max_sent_len=30,
                  author=None,
                  before=None,
-                 after=None,
                  size=32,
                  max_retries=5):
         self.subreddit = subreddit
         self.min_score = min_score
+        self.min_len = min_len
         self.min_sent_len = min_sent_len
         self.max_sent_len = max_sent_len
         self.author = author
         self.before = before
-        self.after = after
         self.size = size
         self.max_retries = max_retries
+        # a praw.ini file is needed in the directory for this scraper to work
         self.reddit = praw.Reddit("scraper1")
         self.nlp = spacy.load("en")
 
@@ -48,16 +49,10 @@ class RedditSpacyExtractor:
             URI_TEMPLATE += "author=%s" % author
             and_flag = True
             
-        if before:
-            if and_flag:
-                URI_TEMPLATE += "&" 
-            URI_TEMPLATE += "before=%s" % before
-            and_flag = True
-            
         if after:
             if and_flag:
                 URI_TEMPLATE += "&"
-            URI_TEMPLATE += "after=%s" % after
+            URI_TEMPLATE += "after=%d" % after
             and_flag = True
             
         if size:
@@ -98,46 +93,58 @@ class RedditSpacyExtractor:
         return more_posts
     
     def get_comments(self):
+        filtered_collections = []
         post_collections = []
+        ids = []
         collection_size = 0
-        if self.after is not None:
-            new_start_at = self.after
+        old_start_at = 0
+        if self.before is not None:
+            new_start_at = math.floor((self.before -
+                                      timedelta(days=365)).timestamp())
         else:
             new_start_at = math.floor((datetime.utcnow() -
                                        timedelta(days=365)).timestamp())
         while collection_size != self.size:
+            if old_start_at == new_start_at:
+                break
             print("starting from :", new_start_at,
                   "post_collections length :", collection_size)
-            retrieved = self.retrieve_json(new_start_at, min(500, self.size - collection_size))
-            post_collections += retrieved
+            retrieved = self.retrieve_json(new_start_at,
+                                           500)
+            print([comment["id"] for comment in retrieved])
+            filtered_collections = [comment for comment in retrieved
+                                    if self.validCommentFilter(comment)
+                                    and comment["id"] not in ids][:self.size]
+            ids += [comment["id"] for comment in filtered_collections]
+            post_collections += filtered_collections
+
+            old_start_at = new_start_at
+
             if collection_size != 0:
                 last = post_collections[-1]
                 new_start_at = last["created_utc"] - (10)
             else:
                 new_start_at -= 10
-            
 
-        return post_collections
+            if self.before:
+                if new_start_at >= self.before:
+                    break
+            collection_size = len(post_collections)
+        return post_collections[:self.size]
 
     def validCommentFilter(self, comment):
         if comment["body"] == "[removed]":
             return False
-        if comment["author"] == "[deleted]":
+        elif comment["author"] == "[deleted]":
             return False
-        if comment["body"] == "[removed]":
+        elif comment["body"] == "[removed]":
             return False
-        if self.reddit.comment(comment["id"]).score < self.min_score:
+        elif len(comment["body"]) < self.min_len:
             return False
-        doc = self.nlp(comment["body"])
-        sents = [sent for sent in doc.sents if len(sent) <= self.max_sent_len]
-        sents = [sent for sent in doc.sents if len(sent) >= self.min_sent_len]
-        # sents = [sent for sent in doc.sents if "\n" not in "".join([tok.text for tok in sent])]
-        # sents = [[token for token in sent] for sent in sents]
-        if len(sents) > 0:
+        elif self.reddit.comment(comment["id"]).score < self.min_score:
+            return False
+        else:
             return True
-        
-        
-        
-        
-            
-            
+
+    def commentCleaner(self, comment):
+        pass

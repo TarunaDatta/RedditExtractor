6b2fad63a6f695357b435ed8f244aa7f4899c817
TarunaDatta <tchittajallu@gmail.com>
2019-11-22 15:03:49 +0530

    made changes to structure of get_comments as to simplify its responsibilities. Instead moved looping logic to get_sentences()

--- redditSpacyExtractor.py
+++ redditSpacyExtractor.py
@@ -1,25 +1,37 @@
 import math
 import requests
-import itertools
-import numpy as np
+import collections
 import praw
 import time
 from datetime import datetime, timedelta
-
+import string
+import pickle
+import os
+# import re
 import spacy
+import argparse
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk import download
+
+
+
 
 
 class RedditSpacyExtractor:
     def __init__(self,
                  subreddit=None,
-                 min_score=5,
-                 min_len=10,
-                 min_sent_len=5,
-                 max_sent_len=30,
+                 min_score=4,
+                 min_len=8,
+                 min_sent_len=6,
+                 max_sent_len=32,
                  author=None,
                  before=None,
                  size=32,
-                 max_retries=5):
+                 max_retries=5,
+                 counter_file="vocab_counter.pkl",
+                 collectionp_file="collection.pkl",
+                 sentence_file="sentences.txt",
+                 spacy_use=False):
         self.subreddit = subreddit
         self.min_score = min_score
         self.min_len = min_len
@@ -31,7 +43,30 @@ class RedditSpacyExtractor:
         self.max_retries = max_retries
         # a praw.ini file is needed in the directory for this scraper to work
         self.reddit = praw.Reddit("scraper1")
-        self.nlp = spacy.load("en")
+        if spacy_use:
+            spacy.prefer_gpu()
+            self.nlp = spacy.load("en")
+        else:
+            download('punkt')
+        self.spacy_use = spacy_use
+        self.counter = collections.Counter()
+        self.commentids = []
+        self.after = None
+        
+        self.collectionp_file = collectionp_file
+        self.counter_file = counter_file
+        self.sentence_file = sentence_file
+
+        self.sent_count = 0
+
+        if os.path.isfile(self.collectionp_file):
+            self.collectionp = pickle.load(open(self.collectionp_file, "rb"))
+            self.commentids = self.collectionp["commentids"]
+            self.after = self.collectionp["after"]
+            self.sent_count = self.collectionp["sentence_count"]
+
+        if os.path.isfile(self.counter_file):
+            self.counter = pickle.load(open(self.counter_file, "rb"))
 
     def URI_from_fields(self, subreddit, author, before, after, size):
         URI_TEMPLATE = "https://api.pushshift.io/reddit/search/comment/?"
@@ -91,46 +126,13 @@ class RedditSpacyExtractor:
                             size=count)
         more_posts = self.make_request(uri)["data"]
         return more_posts
-    
+
     def get_comments(self):
-        filtered_collections = []
-        post_collections = []
-        ids = []
-        collection_size = 0
-        old_start_at = 0
-        if self.before is not None:
-            new_start_at = math.floor((self.before -
-                                      timedelta(days=365)).timestamp())
-        else:
-            new_start_at = math.floor((datetime.utcnow() -
-                                       timedelta(days=365)).timestamp())
-        while collection_size != self.size:
-            if old_start_at == new_start_at:
-                break
-            print("starting from :", new_start_at,
-                  "post_collections length :", collection_size)
-            retrieved = self.retrieve_json(new_start_at,
-                                           500)
-            print([comment["id"] for comment in retrieved])
-            filtered_collections = [comment for comment in retrieved
-                                    if self.validCommentFilter(comment)
-                                    and comment["id"] not in ids][:self.size]
-            ids += [comment["id"] for comment in filtered_collections]
-            post_collections += filtered_collections
-
-            old_start_at = new_start_at
-
-            if collection_size != 0:
-                last = post_collections[-1]
-                new_start_at = last["created_utc"] - (10)
-            else:
-                new_start_at -= 10
-
-            if self.before:
-                if new_start_at >= self.before:
-                    break
-            collection_size = len(post_collections)
-        return post_collections[:self.size]
+        print("starting from :", self.after,
+              "post_collections length :", self.sent_count)
+        retrieve_size = min(self.size, 500)
+        retrieved = self.retrieve_json(self.after, retrieve_size)
+        return retrieved
 
     def validCommentFilter(self, comment):
         if comment["body"] == "[removed]":
@@ -143,8 +145,129 @@ class RedditSpacyExtractor:
             return False
         elif self.reddit.comment(comment["id"]).score < self.min_score:
             return False
+        elif "Bot" in comment["body"] or "Github" in comment["body"] or ".com" in comment["body"] or ".org" in comment["body"]:
+            return False
         else:
             return True
 
     def commentCleaner(self, comment):
-        pass
+        sents = sent_tokenize(comment["body"])
+        sents = [[token for token in word_tokenize(sent)
+                  if token not in string.punctuation]
+                 for sent in sents]
+        sents = [sent for sent in sents
+                 if len(sent) >= self.min_sent_len
+                 and len(sent) <= self.max_sent_len]
+        for sent in sents:
+            for word in sent:
+                self.counter[word] += 1
+        sents = [" ".join(sent) for sent in sents]
+        return sents
+
+    def spacyCleaner(self, comment):
+        doc = self.nlp(comment["body"])
+        sents = list(doc.sents)
+        sents = [[token.text for token in sent
+                  if token.text not in string.punctuation]
+                 for sent in sents]
+        for sent in sents:
+            for word in sent:
+                self.counter[word] += 1
+        sents = [" ".join(sent) for sent in sents]
+        return sents
+
+    def get_sentences(self):
+        if self.before:
+            self.after = math.floor((self.before -
+                                     timedelta(days=365)).timestamp())
+        else:
+            self.after = math.floor((datetime.now() -
+                                     timedelta(days=365)).timestamp())
+        while self.sent_count < self.size:
+            print("Retrieving comments")
+            comments = self.get_comments()
+            print("Filtering comments")
+            comments = [comment for comment in comments
+                        if self.validCommentFilter(comment)
+                        and comment["id"] not in self.commentids]
+            self.commentids += [comment["id"] for comment in comments]
+
+            if len(comments) != 0:
+                last = comments[-1]
+                self.after = last["created_utc"] - (100)
+            else:
+                self.after -= 100
+
+            print("Saving collectionp")
+            with open(self.collectionp_file, "wb") as pklfile:
+                self.collectionp = {"after": self.after,
+                                    "commentids": self.commentids,
+                                    "sentence_count": self.sent_count}
+                pickle.dump(self.collectionp, pklfile)
+
+            print("Saving counter")
+            with open(self.counter_file, "wb") as pklfile:
+                pickle.dump(self.counter, pklfile)
+
+            print("Cleaning sentences")
+            if self.spacy_use:
+                print("Using Spacy")
+                cleaned_sents = [self.commentCleaner(comment)
+                                 for comment in comments]
+            else:
+                print("Using NLTK")
+                cleaned_sents = [self.commentCleaner(comment)
+                                 for comment in comments]
+
+            sents = [sent for sent in cleaned_sents if len(sent) != 0]
+            with open(self.sentence_file, "a+") as sent_file:
+                for line in sents:
+                    for sent in line:
+                        sent_file.write(sent + "\n")
+                        self.sent_count += 1
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extractor options")
+    parser.add_argument("--subreddit", type=str, default=None,
+                        help="subreddit from which to extract")
+    parser.add_argument("--min_score", type=int, default=4,
+                        help="minimum score for comment to be extracted")
+    parser.add_argument("--min_len", type=int, default=8,
+                        help="minimum number of characters in the comment, needed to filter one word comments and such")
+    parser.add_argument("--min_sent_len", type=int, default=6,
+                        help="minimum number of words in the sentence")
+    parser.add_argument("--max_sent_len", type=int, default=32,
+                        help="maximum number of words in the sentence")
+    parser.add_argument("--author", type=str, default=None,
+                        help="filter by specific author")
+    parser.add_argument("--before", type=str, default=None,
+                        help="timestamp that signifies end point of extraction window")
+    parser.add_argument("--size", type=int, default=32,
+                        help="Number of comments to be extracted")
+    parser.add_argument("--max_retries", type=int, default=5,
+                        help="Maximum number of retries before giving up")
+    parser.add_argument("--counter_file", type=str, default="vocab_counter.pkl",
+                        help="The pickle file containing counter information")
+    parser.add_argument("--collectionp_file", type=str, default="collection.pkl",
+                        help="Pickle file containing collection progress")
+    parser.add_argument("--sentence_file", type=str, default="sentences.txt",
+                        help="File name for the output file")
+    parser.add_argument("--spacy_use", type=bool, default=False,
+                        help="Whether or not to use spacy")
+    args = parser.parse_args()
+    print(args)
+    ex = RedditSpacyExtractor(subreddit=args.subreddit,
+                              min_score=args.min_score,
+                              min_len=args.min_len,
+                              min_sent_len=args.min_sent_len,
+                              max_sent_len=args.max_sent_len,
+                              author=args.author,
+                              before=args.before,
+                              size=args.size,
+                              max_retries=args.max_retries,
+                              counter_file=args.counter_file,
+                              collectionp_file=args.collectionp_file,
+                              sentence_file=args.sentence_file,
+                              spacy_use=args.spacy_use)
+    ex.get_sentences()
